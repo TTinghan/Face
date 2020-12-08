7745fcf1292bd22fcd0eb8bd32ea398e1505ff88
tingting.han <286681763@qq.com>
2020-12-08 12:18:21 +0800

    update some text

--- someDocTest/clone.js
+++ someDocTest/clone.js
@@ -6,7 +6,7 @@
  * 枚举+递归
  */
  function cloneDeep(target) {
-     if(target == null || target !== 'object') return obj;
+     if(target == null || target !== 'object') return target;
      const newObj = new target.constructor(); //返回创建实例对象的 Object 构造函数的引用， 无需判断Function或ExpReg
      for( let key in Object.getOwnPropertyDescriptor(target)){ // 获取直接赋予该对象的属性，不需要从原型链上进行查找的属性
         newObj[key] = cloneDeep(target[key])
@@ -14,18 +14,18 @@
      return newObj
  }
 
-
 // 手动写出一个浅克隆 --》
 // 创建一个新的对象，遍历需要克隆的对象，
 // 将需要克隆对象的属性依次添加到新对象上，
 // 返回。
-function clone(target) {
-     let cloneTarget = {}; 
-     for (const key in target) { 
-         cloneTarget[key] = target[key];
-         }
-          return cloneTarget; 
-    };
+function clone(obj){
+    if(obj == null || obj !== 'object') return obj;
+    let cloneTarget = {};
+    for(let key in obj) {
+        cloneTarget[key] = obj[key]
+    }
+    return cloneTarget;
+}
 // 手动实现一个深克隆 --》如果有更深层次的对象可以继续递归直到属性为原始类型
 // JSON.parse(JSON.stringify()); 
 // 如果是原始类型，无需继续拷贝，直接返回
