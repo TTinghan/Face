75cb1a955b1886bc9b2a978a3166408e22a4a2bd
hantingting03 <hantingting03@kuaishou.com>
2023-06-11 20:16:02 +0800

    chore: update

--- practice/202306/leetcode.js
+++ practice/202306/leetcode.js
@@ -1,10 +1,25 @@
 // startsWith 判断当前字符串是否以另外一个给定的子字符串开头 返回一个布尔值
 // sort toSorted
+
 // shift pop
-// Math.max()
+// unshift push
 
+// Math.max() 最大值
+// Math.abs() 绝对值
+// Math.ceil() 四舍五入 向上取整
+// Math.floor() 四舍五入 向下取整
+// Math.random() 0-1随机数
 
+// 防止大数计算导致的溢出问题--》someLargeNumber % 1e9+7
+// 取模（mod）:取模操作使用%运算符，例如a % b表示a除以b后的余数
+// 取模的操作是根据除数的符号来决定余数的符号
 
+// 取余（rem）:取余操作使用std::rem函数，例如std::rem(a, b)表示a除以b后的余数，结果的符号与a的符号相同
+// 取余的操作是根据被除数的符号来决定余数的符号
+// 1e9+7
+
+// Array.from()
+// Array.fill(0)
 
 
 
@@ -49,3 +64,19 @@ var widthOfBinaryTree = function(root) {
  
      return max_width;
  };
+
+//  动态规划-》n首不同的歌，一共听goal首，每首支少播放一次，一首歌只有在其他 k 首歌播放完之后才能再次播放
+ var numMusicPlaylists = function(n, goal, k) {
+    const mod = 1e9 + 7;
+    let dp = Array.from({ length: goal + 1 }, () => Array(n + 1).fill(0)); // 创建二维数组
+    dp[0][0] = 1;
+    for (let i = 1; i <= goal; i++) { // i是已经播放了i首歌
+        for (let j = 1; j <= n; j++) { // j是上一首播放的歌曲是什么
+            dp[i][j] = dp[i - 1][j - 1] * (n - (j - 1)) % mod;
+            if (j > k) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j] * (j - k)) % mod;
+            }
+        }
+    }
+    return dp[goal][n];
+};
