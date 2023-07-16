782de576987c0cc49e6dabf5550adf0100c9a543
hantingting03 <hantingting03@kuaishou.com>
2023-07-16 19:16:18 +0800

    chore: update

--- practice/202307/coding.js
+++ practice/202307/coding.js
@@ -4,3 +4,25 @@ function myNew(func, ...args){
   let result = func.apply(obj, ...args);
   return (result !== null && typeof result === 'object') ? result : obj
 }
+
+function PromiseAll(promises){
+  return new Promise((resolve, reject)=>{
+    if (!Array.isArray(promises)){
+      return reject(new TypeError('arguements is must be array!'))
+    }
+    let promisesLen = promises.length;
+    let resolvedValue = new Array(promisesLen);
+    let resolvedCounter = 0;
+    for(let i = 0; i < promisesLen; i++) {
+      Promise.resolve(promises[i]).then(function(value){
+        resolvedCounter++;
+        resolvedValue[i] = value;
+        if(resolvedCounter === promisesLen) {
+          return resolve(resolvedValue);
+        }
+      }, function(reason){
+        return reject(reason);
+      })
+    }
+  })
+}
